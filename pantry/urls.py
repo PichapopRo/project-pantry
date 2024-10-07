8140d8024e8f0a3a34e30c37a3730d8e25b4fa57
Pichapop Rodduayboon <pichapop.ro@ku.th>
2024-10-07 19:27:11 +0700

    Fixing when entering http://127.0.0.1:8000/ return to the recipe page

--- pantry/urls.py
+++ pantry/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("recipes/", include("webpage.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
+    path('', RedirectView.as_view(url='/recipes/', permanent=True)),
 ]
