9c15e200408a9d74596fa5e68e16a9ecaaacc057
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-10-08 22:05:27 +0700

    Remove unused attribute from the Recipe class

--- webpage/models.py
+++ webpage/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Ingredient(models.Model):
     """An r contains the name, a spoonacauar_id(if exitst) and a link to a picture."""
     name = models.CharField(max_length=100, default='Unnamed Ingredient')
-    spoonacular_id = models.IntegerField(unique=True, default=0, null=True)
+    spoonacular_id = models.IntegerField(unique=True, null=True)
     picture = models.URLField(default='', null=True)
 
     def __str__(self):
@@ -46,10 +46,8 @@ class Recipe(models.Model):
     """
     name = models.CharField(max_length=200, default='Unnamed Recipe')
     spoonacular_id = models.IntegerField(unique=True, null=True)
-    cooking_steps = models.JSONField(default=dict)
     estimated_time = models.FloatField(default=0)
     images = models.ImageField(upload_to='recipe_images/', blank=True)
-    equipment = models.JSONField(default=dict)
     poster_id = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -73,9 +71,9 @@ class Recipe(models.Model):
     def get_ingredients() -> list[Ingredient]:
         """Return a list of ingredients of the recipe.
         
-        :return: A list of ingredients..
+        :return: A list of ingredients.
         """
-        pass 
+        
     
     def get_equipement() -> list[Ingredient]:
         """Return a list of ingredients of the recipe.
