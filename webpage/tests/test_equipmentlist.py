2a902223abc60b9fc2c54aac5b84fe2c6b22e9b6
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-10-21 22:55:30 +0700

    Apply flake8 for test_equipmentlist.py

--- webpage/tests/test_equipmentlist.py
+++ webpage/tests/test_equipmentlist.py
@@ -1,13 +1,23 @@
+"""Tests for the EquipmentList model."""
 from django.test import TestCase
 from django.contrib.auth.models import User
 from webpage.models import Equipment, Recipe, EquipmentList
 
+
 class EquipmentListModelTest(TestCase):
+    """Test the EquipmentList model."""
+
     def setUp(self):
         """Create test data before each test."""
-        self.user = User.objects.create_user(username="testuser", password="testpass")
-        self.recipe = Recipe.objects.create(name="Test Recipe", poster_id=self.user)
-        self.equipment = Equipment.objects.create(name="Oven", spoonacular_id=1)
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass"
+        )
+        self.recipe = Recipe.objects.create(
+            name="Test Recipe", poster_id=self.user
+        )
+        self.equipment = Equipment.objects.create(
+            name="Oven", spoonacular_id=1
+        )
         self.equipment_list = EquipmentList.objects.create(
             equipment=self.equipment,
             recipe=self.recipe,
@@ -23,6 +33,9 @@ class EquipmentListModelTest(TestCase):
         self.assertEqual(self.equipment_list.unit, "piece")
 
     def test_equipmentlist_relationship(self):
-        """Test the relationships between EquipmentList, Recipe, and Equipment."""
+        """
+        Test the relationships between EquipmentList, Recipe, 
+        and Equipment.
+        """
         self.assertEqual(self.equipment_list.equipment.name, "Oven")
         self.assertEqual(self.equipment_list.recipe.name, "Test Recipe")
