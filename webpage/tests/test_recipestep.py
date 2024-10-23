9e196efd823c21f55cccb920f3fad3acb1ec17f1
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-10-23 17:04:36 +0700

    remove some bits from RecipeStepModelTest

--- webpage/tests/test_recipestep.py
+++ webpage/tests/test_recipestep.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 from webpage.models import RecipeStep, Recipe
 import pytest
 
-
+@pytest.mark.django_db
 class RecipeStepModelTest(TestCase):
     """Test suite for the RecipeStep model."""
 
-    @pytest.mark.django_db(transaction=True)
     def setUp(self):
         """
         Create a user, a recipe, and a recipe step instance to use in the tests.
