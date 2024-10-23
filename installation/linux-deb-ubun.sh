a8eac3c81e7d1851586f04e0af2de558da355115
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-10-23 20:40:38 +0700

    Make the scripts create .env files first before migrate

--- installation/linux-deb-ubun.sh
+++ installation/linux-deb-ubun.sh
@@ -7,8 +7,6 @@ virtualenv --python python3.11 venv
 source venv/bin/activate
 echo Installing python requirements
 python -m pip install -r requirements.txt
-echo Initializing Django
-python manage.py migrate
 echo "Initializing environment"
 python -c "from django.core.management.utils import get_random_secret_key; f = open('.env', 'w'); f.write('SECRET_KEY=django-insecure-'+get_random_secret_key()+'\n'); f.close()"
 echo "DEBUG=False" >> .env
@@ -16,3 +14,5 @@ echo "ALLOWED_HOSTS=*" >> .env
 echo "TIME_ZONE=Asia/Bangkok" >> .env
 echo API_KEY=fake-api-key >> .env
 echo SPOONACULAR_PASSWORD=set-your_password >> .env
+echo Initializing Django
+python manage.py migrate
--- installation/mac-os.sh
+++ installation/mac-os.sh
@@ -5,8 +5,6 @@ source venv/bin/activate
 
 echo Installing python requirements
 python -m pip install -r requirements.txt
-echo Initializing Django
-python manage.py migrate
 echo "Initializing environment"
 python -c "from django.core.management.utils import get_random_secret_key; f = open('.env', 'w'); f.write('SECRET_KEY=django-insecure-'+get_random_secret_key()+'\n'); f.close()"
 echo "DEBUG=False" >> .env
@@ -14,3 +12,5 @@ echo "ALLOWED_HOSTS=*" >> .env
 echo "TIME_ZONE=Asia/Bangkok" >> .env
 echo API_KEY=fake-api-key >> .env
 echo SPOONACULAR_PASSWORD=set-your_password >> .env
+echo Initializing Django
+python manage.py migrate
