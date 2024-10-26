0bbf5c3b67192259169c4736568d41f849cb5396
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-10-26 18:30:15 +0700

    Added FilterParam class

--- webpage/modules/filter_objects.py
+++ webpage/modules/filter_objects.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass
 from webpage.models import Recipe, Ingredient, Equipment, Diet, Step
-
+from enum import Enum    
+    
 @dataclass
-class FilterObjects:
-    diet: list[str|None] = []
-    ingredients: list[str|None] = None
+class FilterParam:
+    include: list[dict[str, str|float|int]]
+    exclude: list[dict[str, str|float|int]]
     offset: int = 0
     number: int = 0
+
+    def add_filter(self, filter_name: str, filter_value: int|float|str, include: bool = True):
+        if include:
+            self.include.append({filter_name: filter_value})
+        else:
+            self.exclude.append({filter_name: filter_value})
     
     
     
