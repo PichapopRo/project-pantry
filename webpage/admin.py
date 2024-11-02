2bc98becca1d9a66c738bc675685027bc56998bd
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-11-02 14:41:28 +0700

    Adding status and Admin able to change Recipe's status

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,13 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django import forms
+from .models import Recipe, IngredientList, EquipmentList, NutritionList
 
-from .models import Recipe
 
-# Register your models here.
-@admin.register(Recipe)
+class IngredientListInline(admin.TabularInline):
+    model = IngredientList
+    extra = 1
+    readonly_fields = ['ingredient', 'amount', 'unit']
+    verbose_name = "Ingredient"
+    verbose_name_plural = "Ingredients"
+
+class EquipmentListInline(admin.TabularInline):
+    model = EquipmentList
+    extra = 1
+    readonly_fields = ['equipment', 'amount', 'unit']
+    verbose_name = "Equipment"
+    verbose_name_plural = "Equipment List"
+
+class NutritionListInline(admin.TabularInline):
+    model = NutritionList
+    extra = 1
+    readonly_fields = ['nutrition', 'amount', 'unit']
+    verbose_name = "Nutrition"
+    verbose_name_plural = "Nutrition List"
+
+class RecipeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 6, 'cols': 100}),
+        }
+
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'poster_id', 'created_at')
+    form = RecipeAdminForm
+    list_display = ('name', 'poster_id', 'created_at', 'status')
+    list_filter = ('status',)
+    list_editable = ('status',)
     search_fields = ('name', 'description')
-    ordering = ('created_at',)
-    date_hierarchy = 'created_at'
+    inlines = [IngredientListInline, EquipmentListInline, NutritionListInline]
+
 admin.register(User)
+admin.site.register(Recipe, RecipeAdmin)
