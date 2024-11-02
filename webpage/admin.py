81b28926ba95e003491f6afb2a08e725d43ba6a5
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-11-02 14:04:04 +0700

    Register recipe to admin

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from .models import Recipe
 
 # Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'poster_id', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+admin.register(User)
