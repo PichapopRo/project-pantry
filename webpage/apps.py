339242cdfaf7cd8fd2806c6f87b20c9d3f4c8c8e
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-11-06 15:58:03 +0700

    Adding docstrings in apps.py

--- webpage/apps.py
+++ webpage/apps.py
@@ -1,9 +1,12 @@
+"""Import the essential package for AppConfig."""
 from django.apps import AppConfig
 
 
 class WebpageConfig(AppConfig):
+    """App config for the web app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'webpage'
 
     def ready(self):
+        """Import signals after making change in the admin page."""
         import webpage.signals
