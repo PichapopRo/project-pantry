0b7c2409a906d7e9a05b8ebb5d9ddb3e4fef3f0a
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-11-11 20:31:39 +0700

    Adding settings for google auth inside settings.py

--- pantry/settings.py
+++ pantry/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webpage.apps.WebpageConfig'
+    'django.contrib.sites'
+    'webpage.apps.WebpageConfig',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
