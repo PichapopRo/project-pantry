8558c8901e7475da81e11bced4518405ba31764b
Pattharamon D. <pattharamon.d@ku.th>
2024-11-20 11:04:47 +0700

    added codes

---
+++ webpage/tests/test_filter_objects.py
@@ -0,0 +1,9 @@
+"""Tests for the FilterParam class."""
+
+from django.test import TestCase
+from webpage.modules.filter_objects import FilterParam
+
+
+class GetDataProxyTest(TestCase):
+    """"""
+    
