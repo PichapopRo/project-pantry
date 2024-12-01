592b93b47118f8181a2fef25991a6d726fada398
Pattharamon D. <pattharamon.d@ku.th>
2024-12-01 16:04:39 +0700

    fixed flake8

--- s_webdriver.py
+++ s_webdriver.py
@@ -1,24 +1,21 @@
+"""Using Selenium WebDriver to navigate to website when running."""
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
-def main():
-    service = Service(log_path="NUL")
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
+service = Service(log_path="NUL")
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-logging")
+options.add_argument("--log-level=3")
 
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://project-pantry.onrender.com/")
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://project-pantry.onrender.com/")
 
-    try:
-        while driver.window_handles:
-            pass
-    except KeyboardInterrupt:
-        print("Exiting script...")
-    finally:
-        driver.quit()
-
-
-if __name__ == "__main__":
-    main()
+try:
+    while driver.window_handles:
+        pass
+except KeyboardInterrupt:
+    print("Exiting script...")
+finally:
+    driver.quit()
