08fa5ed7a9fba06c3da38326adcff29397a41426
Pattharamon D. <pattharamon.d@ku.th>
2024-12-03 01:21:56 +0700

    fixed codes

--- seleniumfiles/s_webdriver.py
+++ seleniumfiles/s_webdriver.py
@@ -3,11 +3,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from decouple import config
+import subprocess
+import time
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("sWebdriver")
-LINK_URL = config('LINK_URL', default='http://127.0.0.1:8000/recipes/')
+LINK_URL = config('LINK_URL', default=None)
+
+if LINK_URL is None:
+    logger.info("Starting Django development server.")
+    subprocess.Popen(['python', 'manage.py', 'runserver'])
+    LINK_URL = 'http://127.0.0.1:8000/recipes/'
+    time.sleep(5)
 
 service = Service()
 options = webdriver.ChromeOptions()
